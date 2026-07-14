543384d981634442671b473ab6b2b6cc0ed19e20
Jose A Munoz Cardozo <jamc221996@gmail.com>
2026-07-13 22:56:15 -0400

    Agregar CSP meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Forzar al navegador a transformar HTTP en HTTPS -->
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'CRM Operativo') }}</title>
     @viteReactRefresh
