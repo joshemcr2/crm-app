f86d66eb49cb0501d06c3aaf36e49281e1866d81
Jose A Munoz Cardozo <jamc221996@gmail.com>
2026-07-13 23:10:06 -0400

    Forzar URL scheme a https en produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,19 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // <-- Agregamos esto
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        // Forzar HTTPS en entornos de producción como Railway
-        if (config('app.env') === 'production' || config('app.env') === 'local') {
+        // Forzar HTTPS en producción si no estamos en la consola local
+        if (config('app.env') === 'production' || !app()->runningInConsole()) {
             URL::forceScheme('https');
         }
     }
